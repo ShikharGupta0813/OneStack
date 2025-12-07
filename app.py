f7ca95587cd03f3750f2cfcd8c4272639fc43628
ShikharGupta0813 <shikhargupta484@gmail.com>
2025-12-07 23:48:04 +0530

    deployement fix

--- app.py
+++ app.py
@@ -5,16 +5,13 @@ from routes.upload_route import upload_bp
 from routes.data_route import data_bp
 from database.db import init_db
 
-
 def create_app():
     app = Flask(__name__)
 
-    # Enable CORS for frontend (React)
-    # CORS(app, resources={r"/*": {"origins": "*"}})
-    
+    # CORS
     CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
-    # Register blueprints with URL prefixes
+    # Blueprints
     app.register_blueprint(upload_bp, url_prefix="/api")
     app.register_blueprint(data_bp, url_prefix="/api")
 
@@ -22,12 +19,9 @@ def create_app():
     def home():
         return jsonify({"message": "InsightDocs PDF Extraction API Running"})
 
-    # Initialize database
+    # Initialize DB
     init_db()
 
     return app
 
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True, port=5000)
+app = create_app()
